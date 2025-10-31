f4d3a6403ce8574b9b38835d55ada24f436d607e
Jeremy <jlsemelfort@gmail.com>
2025-10-31 15:26:56 -0400

    lab9 finish

--- library/catalog/views.py
+++ library/catalog/views.py
@@ -14,17 +14,22 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
     # The 'all()' is implied by default.
-    num_authors = Author.objects.count()
+    num_authors = Author.objects.count()  # The 'all()' is implied by default.
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
-        'num_genres': num_genres,
+        'num_visits': num_visits,
     }
 
-    # Render the HTML template index.html with the data in the context variable
+    # Render the HTML template index.html with the data in the context variable.
     return render(request, 'index.html', context=context)
     
 class BookListView(generic.ListView):
